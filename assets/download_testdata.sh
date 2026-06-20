03a220b15e4938150e2ed3c0a8f1e38032efc4ac
carolienv <carolien.vlieghe@irc.vib-ugent.be>
2026-06-20 14:15:34 +0200

    Create second testsample with symlink and run test automatically

--- assets/download_testdata.sh
+++ assets/download_testdata.sh
@@ -12,10 +12,41 @@ OUTDIR="assets/test_data/10x_5prime_pbmc_1k"
 
 mkdir -p "${OUTDIR}"
 
-wget -c \
-  -O "${OUTDIR}/sc5p_v2_hs_PBMC_1k_possorted_genome_bam.bam" \
-  "https://cf.10xgenomics.com/samples/cell-vdj/4.0.0/sc5p_v2_hs_PBMC_1k/sc5p_v2_hs_PBMC_1k_possorted_genome_bam.bam"
+BAM="sc5p_v2_hs_PBMC_1k_possorted_genome_bam.bam"
+BAI="${BAM}.bai"
 
-wget -c \
-  -O "${OUTDIR}/sc5p_v2_hs_PBMC_1k_possorted_genome_bam.bam.bai" \
-  "https://cf.10xgenomics.com/samples/cell-vdj/4.0.0/sc5p_v2_hs_PBMC_1k/sc5p_v2_hs_PBMC_1k_possorted_genome_bam.bam.bai"
+BAM_URL="https://cf.10xgenomics.com/samples/cell-vdj/4.0.0/sc5p_v2_hs_PBMC_1k/${BAM}"
+BAI_URL="https://cf.10xgenomics.com/samples/cell-vdj/4.0.0/sc5p_v2_hs_PBMC_1k/${BAI}"
+
+S2_BAM="s2_${BAM}"
+S2_BAI="${S2_BAM}.bai"
+
+if [ ! -f "${OUTDIR}/${BAM}" ]; then
+    echo "Downloading ${BAM}..."
+    wget -O "${OUTDIR}/${BAM}" "${BAM_URL}"
+else
+    echo "${BAM} already exists, skipping download."
+fi
+
+if [ ! -f "${OUTDIR}/${BAI}" ]; then
+    echo "Downloading ${BAI}..."
+    wget -O "${OUTDIR}/${BAI}" "${BAI_URL}"
+else
+    echo "${BAI} already exists, skipping download."
+fi
+
+if [ ! -L "${OUTDIR}/${S2_BAM}" ]; then
+    echo "Creating symlink ${S2_BAM}..."
+    ln -s "${BAM}" "${OUTDIR}/${S2_BAM}"
+else
+    echo "${S2_BAM} symlink already exists, skipping."
+fi
+
+if [ ! -L "${OUTDIR}/${S2_BAI}" ]; then
+    echo "Creating symlink ${S2_BAI}..."
+    ln -s "${BAI}" "${OUTDIR}/${S2_BAI}"
+else
+    echo "${S2_BAI} symlink already exists, skipping."
+fi
+
+echo "Done."
--- main.nf
+++ main.nf
@@ -14,6 +14,29 @@ include { RUN_OPTITYPE } from './modules/local/optitype/optitype'
 include { COLLECT_OPTITYPE_RESULTS } from './modules/local/optitype/postprocessing'
 workflow {
 
+    if (params.prepare_test_data) {
+        log.info "Preparing test data with assets/download_testdata.sh"
+
+        def stdout = new StringBuffer()
+        def stderr = new StringBuffer()
+
+        def proc = ["bash", "${projectDir}/assets/download_testdata.sh"].execute()
+        proc.consumeProcessOutput(stdout, stderr)
+        def exit_code = proc.waitFor()
+
+        if (stdout) {
+            log.info stdout.toString()
+        }
+
+        if (stderr) {
+            log.warn stderr.toString()
+        }
+
+        if (exit_code != 0) {
+            error "Test data preparation failed with exit code ${exit_code}"
+        }
+    }
+
     samples_ch = Channel
         .fromPath(params.input)
         .splitCsv(header: true)
